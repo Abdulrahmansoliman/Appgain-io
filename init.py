eb55338cf7178d68459c33ef7d77b2c48c39461c
Abdulrahman Soliman <abdulrahmansoliman391@gmail.com>
2023-06-02 05:30:20 +0300

    Added GET, POST, PUT methods for shortlinks

--- init.py
+++ init.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from models.init import db
+import mongoengine
 
 app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongodb+srv://abdulrahmansoliman391:<1234>@cluster0.afo6bau.mongodb.net/?retryWrites=true&w=majority'
-}
+    'host': 'mongodb+srv://abdulrahmansoliman391:1234@cluster0.afo6bau.mongodb.net/?retryWrites=true&w=majority',
+    'db': 'shortlinks'}
 
 db = MongoClient(app.config['MONGODB_SETTINGS']
                  ['host'], server_api=ServerApi('1'))
@@ -19,6 +19,15 @@ try:
 except Exception as e:
     print(e)
 
+# Set the default connection for mongoengine
+mongoengine.connect(app.config['MONGODB_SETTINGS']
+                    ['db'], host=app.config['MONGODB_SETTINGS']['host'])
+with app.app_context():
+    from routes.shorlinks.shortlinks import shortlinks_bp
+    app.register_blueprint(shortlinks_bp, url_prefix='/shortlinks')
+
+
 if __name__ == '__main__':
     # DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000, debug=True)
+
--- models/shortlinks/shortlinks.py
+++ models/shortlinks/shortlinks.py
@@ -1,8 +1,21 @@
-from models.init import *
-
+from models.shortlinks.init import db
 
 class ShortLink(db.Document):
     slug = db.StringField(unique=True, required=True)
     ios = db.DictField(required=True)
     android = db.DictField(required=True)
     web = db.StringField(required=True)
+     
+    def format(self):
+        return {
+            'slug': self.slug,
+            'ios': {
+                'primary': self.ios.get('primary', ''),
+                'fallback': self.ios.get('fallback', '')
+            },
+            'android': {
+                'primary': self.android.get('primary', ''),
+                'fallback': self.android.get('fallback', '')
+            },
+            'web': self.web
+        }
